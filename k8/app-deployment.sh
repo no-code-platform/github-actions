63ae99afd17acf895680124fdc10ff2ee8b0ddd1
Pathik Devani <pathikdevani@gmail.com>
2023-04-15 14:42:12 +0400

    changed

--- k8/app-deployment.sh
+++ k8/app-deployment.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 PR_NUMBER=$1
-MANIFESTS_DIR=.
 
 # Substitute PR number in the deployment manifest
-cat $MANIFESTS_DIR/app-deployment.yaml | \
+cat app-deployment.yaml | \
   sed "s/PR_NUMBER_PLACEHOLDER/$PR_NUMBER/g" | \
   kubectl apply -f -
 
