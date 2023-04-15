293bc4d81c1b925348ebbc7a0eca9f6e2157ef5f
Pathik Devani <pathikdevani@gmail.com>
2023-04-15 15:02:38 +0400

    changed

--- k8/app-deployment.sh
+++ k8/app-deployment.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-PR_NUMBER=$1
+PR_NUMBER="pr-$1"
+
+cd $(dirname "$0")
+
+kubectl delete deployments "myapp-pr-$PR_NUMBER"
 
 # Substitute PR number in the deployment manifest
-cat app-deployment.yaml | \
+cat ./app-deployment.yaml | \
   sed "s/PR_NUMBER_PLACEHOLDER/$PR_NUMBER/g" | \
   kubectl apply -f -
+
 
 # Wait for the deployment to be ready
 kubectl wait --for=condition=Available deployment/myapp-pr-$PR_NUMBER
